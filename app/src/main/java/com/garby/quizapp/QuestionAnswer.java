71731ca4d12c17d6f71f677ef92194794ec168e7
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-02-10 14:50:32 +0100

    Create QuestionAnswer.java

---
+++ app/src/main/java/com/garby/quizapp/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.garby.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which fintech company owns Barter?",
+            "Which of these is not a programming language?",
+            "Which continent is Nigeria located in?",
+            "Which fintech company owns PocketMoni?"
+    };
+
+    public static String choices[][] = {
+            {"VGC","Softcom","Cellulant","Flutterwave"},
+            {"Golang","Kotlin","Zoom","Python"},
+            {"Europe","Asia","Africa","Antartica"},
+            {"Kuda","Interswitch","eTranzact","Carbon"}
+    };
+
+    public static String correctAnswers[] = {
+            "Flutterwave",
+            "Zoom",
+            "Africa",
+            "eTranzact"
+    };
+
+}
